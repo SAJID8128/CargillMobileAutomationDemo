e66e8053809feaaca06e3421efbe03a142479661
SAJID <sajidhuda73@gmail.com>
2021-10-27 20:50:48 -0400

    First Commit

--- src/test/java/com/cargill/mobile/qa/pages/LoginPage.java
+++ src/test/java/com/cargill/mobile/qa/pages/LoginPage.java
@@ -1,6 +1,43 @@
 package com.cargill.mobile.qa.pages;
 
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AndroidFindBys;
+
 public class LoginPage extends BasePage {
+
+    @AndroidFindBy(accessibility = "test-Username")
+    private MobileElement userNameEditText;
+
+    @AndroidFindBy(accessibility = "test-Password")
+    private MobileElement passwordEditText;
+
+    @AndroidFindBy(accessibility = "test-LOGIN")
+    private MobileElement btnLogin;
+
+    @AndroidFindBy(accessibility = "test-Error message")
+    private MobileElement errorMessageTextView;
+
+    // "return this" returns "LoginPage" class
+    // Calling "clear" method from BasePage
+    // Calling "sendKeys" method from BasePage
+    public LoginPage enterUserName(String username) throws InterruptedException{
+        clear(userNameEditText);
+        sendKeys(userNameEditText, username);
+        return this;
+    }
+
+    public LoginPage enterPassword(String password) throws InterruptedException{
+        clear(passwordEditText);
+        sendKeys(passwordEditText, password);
+        return this;
+    }
+
+    public void pressLogBtn() {
+        click(btnLogin);
+
+    }
+
 
 
 }
--- src/test/java/com/cargill/mobile/qa/runners/MyTestRunner.java
+++ src/test/java/com/cargill/mobile/qa/runners/MyTestRunner.java
@@ -1,8 +1,11 @@
 package com.cargill.mobile.qa.runners;
 
+import com.cargill.mobile.qa.pages.LoginPage;
 import com.cargill.mobile.qa.utils.DriverManager;
 import com.cargill.mobile.qa.utils.GlobalParams;
 import com.cargill.mobile.qa.utils.ServerManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 /* Note - If we call these 3 things; and they are all correct, then we can hit one method from the "LoginPage".
     1) GlobalParams
@@ -10,12 +13,40 @@ import com.cargill.mobile.qa.utils.ServerManager;
     3) ServerManager */
 // First we have to call "GlobalParams" and then call "initializeGlobalParams".
 public class MyTestRunner {
+
+    @BeforeClass
+    public static void initialize() throws Exception {
+        GlobalParams params = new GlobalParams();
+        params.initializeGlobalParams();
+        ServerManager serverManager = new ServerManager();
+        serverManager.startServer();
+        DriverManager driverManager = new DriverManager();
+        driverManager.initializeDriver();
+    }
+
+    @AfterClass
+    public static void quit() {
+        DriverManager driverManager = new DriverManager();
+        if (driverManager.getDriver() != null) {
+            driverManager.getDriver().quit();
+            driverManager.setDriver(null);
+        }
+
+        ServerManager serverManager = new ServerManager();
+        if (serverManager.getServer() != null) {
+            serverManager.getServer().stop();
+        }
+    }
+}
+
+
+// NOTE:---------- Everything below is done above using Cucumber/JUnit ----------
     /* First we created an object called "params" and then using this object we called the method
        "initializeGlobalParams" so that all our params will be properly set. */
     /* Then we call the server manager by calling "startServer" which will set up the 3 environment variables;
        then "GlobalParams" has all the params which we will be taking to create one "AppiumServiceBuilder".
        Now our server is properly configured with my system.*/
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         GlobalParams params = new GlobalParams();
         params.initializeGlobalParams();
 
@@ -26,11 +57,16 @@ public class MyTestRunner {
         try {
             DriverManager driverManager = new DriverManager();
             driverManager.initializeDriver();
-            driverManager.getDriver().quit();
-            serverManager.getServer().stop();
+
+            LoginPage loginPage = new LoginPage();
+            loginPage.enterUserName("Nyla-8128");
+            loginPage.enterPassword("supergirl");
+            loginPage.pressLogBtn();
+            //driverManager.getDriver().quit();
+           // serverManager.getServer().stop();
 
         } catch (Exception ex) {
 
         }
-    }
-}
+    }*/
+
