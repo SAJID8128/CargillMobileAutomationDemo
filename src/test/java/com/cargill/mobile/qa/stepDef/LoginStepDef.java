f589b52bbdb9c7c7b30dccca0e4847797ec0b310
SAJID <sajidhuda73@gmail.com>
2021-11-04 20:23:24 -0400

    First Commit

--- src/test/java/com/cargill/mobile/qa/stepDef/LoginStepDef.java
+++ src/test/java/com/cargill/mobile/qa/stepDef/LoginStepDef.java
@@ -1,17 +1,22 @@
 package com.cargill.mobile.qa.stepDef;
 
 import com.cargill.mobile.qa.pages.LoginPage;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 
 public class LoginStepDef {
-
-
 
     @When("I enter username as {string}")
     public void i_enter_username_as(String string) {
         LoginPage loginPage = new LoginPage();
         loginPage.enterUserName(string);
     }
+
+//    @And("I enter password as {String}")
+//    public void i_enter_password_as(String string) {
+//        LoginPage loginPage = new LoginPage();
+//        loginPage.enterPasswordName(string);
+//    }
 
 
 }
--- src/test/resources/Login.feature
+++ src/test/resources/Login.feature
@@ -3,8 +3,11 @@ Feature: Login scenarios
 
   Scenario Outline: Login with invalid user name
     When I enter username as "<username>"
+#    And I enter password as "<password>"
+#    And I login
+#    Then login should fain with "<err>"
 
     Examples:
     | username |
-    | Nyla     |
+    | Nyla |
 
